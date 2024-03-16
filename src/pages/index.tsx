854a24e50f7414084c34adae68fe1e31f34ddf09
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-17 04:20:24 +0900

    :sparkles: QiitaのAPIキーをフォームで入力できるようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,34 +4,24 @@ import { articles } from '../constants/articles'
 import Link from 'next/link'
 import axios from 'axios'
 import { Article } from '@/types/Article'
-
-const config = {
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_QIITA_TOKEN}`,
-  },
-}
-
-const fetchArticles = async (title: string): Promise<Article[]> => {
-  // TODO: configの型定義を追加する
-  // TODO: 検索条件は最終的にはオブジェクトとかにまとめて引数として渡すようにする
-  const query = `title:${title}`
-  const res = await axios.get<Article[]>(
-    `https://qiita.com/api/v2/items?per_page=5&query=${query}`,
-    config
-  )
-  return res.data
-}
+import { useRecoilState } from 'recoil'
+import { qiitaApiTokenState } from '@/state/qiitaApiTokenState'
 
 export default function Home() {
   const [articles, setArticles] = useState<Article[]>([])
   const [title, setTitle] = useState<string>('')
+  const [qiitaApiToken, setQiitaApiToken] =
+    useRecoilState<string>(qiitaApiTokenState)
 
   const handleInputTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value)
   }
 
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleInputApi = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQiitaApiToken(e.target.value)
+  }
+
+  const handleTitleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault() // フォームが送信されてリロードされないよう
     console.log(title)
     fetchArticles(title).then((articles) => {
@@ -39,11 +29,30 @@ export default function Home() {
     })
   }
 
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${qiitaApiToken}`,
+    },
+  }
+
+  const fetchArticles = async (title: string): Promise<Article[]> => {
+    // TODO: configの型定義を追加する
+    // TODO: 検索条件は最終的にはオブジェクトとかにまとめて引数として渡すようにする
+    // TODO: エラーハンドリングを追加する(APIキーがない場合など)
+    const query = `title:${title}`
+    const res = await axios.get<Article[]>(
+      `https://qiita.com/api/v2/items?per_page=5&query=${query}`,
+      config
+    )
+    return res.data
+  }
+
   useEffect(() => {
     // NOTE: 初期表示時に記事を取得する必要はないかもしれない
-    fetchArticles(title).then((articles) => {
-      setArticles(articles)
-    })
+    // fetchArticles(title).then((articles) => {
+    //   setArticles(articles)
+    // })
   }, [])
   return (
     <>
@@ -62,14 +71,21 @@ export default function Home() {
               type="text"
               placeholder="記事タイトル"
             />
-            <button onClick={handleClick}>検索</button>
+            <button onClick={handleTitleClick}>検索</button>
           </form>
         </div>
         <div>
           <form action="">
-            {/* TODO: APIキーの入力部分は最終的にはモーダルで実装したい */}
-            <input type="text" placeholder="APIキー" />
-            <button>APIキーを入力</button>
+            {/*
+            TODO: APIキーの入力部分は最終的にはモーダルで実装したい
+            TODO:　正規表現を用いてAPIキーの形式をチェックする
+            */}
+            <input
+              onChange={handleInputApi}
+              type="text"
+              placeholder="APIキー"
+            />
+            <div>{qiitaApiToken}</div>
           </form>
         </div>
         <div>

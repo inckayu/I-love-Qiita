50f47139b6c9bcdf866e8b052d0d2f6fb3ea4378
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-17 04:36:21 +0900

    :technologist: 開発環境でqiitaApiTokenStateの初期値を設定した

--- src/state/qiitaApiTokenState.ts
+++ src/state/qiitaApiTokenState.ts
@@ -2,5 +2,8 @@ import { atom } from 'recoil'
 
 export const qiitaApiTokenState = atom<string>({
   key: 'qiitaApiTokenState',
-  default: '',
+  default:
+    process.env.NODE_ENV === 'development'
+      ? process.env.NEXT_PUBLIC_QIITA_TOKEN
+      : '',
 })

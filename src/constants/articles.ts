2d4997dd15e8309042c45d175f8fadd861185886
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-17 00:01:36 +0900

    :adhesive_bandage: urlを適当な文字列に変更

--- src/constants/articles.ts
+++ src/constants/articles.ts
@@ -2,7 +2,7 @@ export const articles = [
   {
     id: '1',
     title: '記事タイトル1',
-    url: 'https://qiita.com/1',
+    url: 'xxxxx.jp',
     likes: 100,
     comments: 10,
     createdAt: '2021-01-01T00:00:00Z',
@@ -10,7 +10,7 @@ export const articles = [
   {
     id: '2',
     title: '記事タイトル2',
-    url: 'https://qiita.com/2',
+    url: 'xxxxx.jp',
     likes: 200,
     comments: 20,
     createdAt: '2021-01-02T00:00:00Z',
@@ -18,7 +18,7 @@ export const articles = [
   {
     id: '3',
     title: '記事タイトル3',
-    url: 'https://qiita.com/3',
+    url: 'xxxxx.jp',
     likes: 300,
     comments: 30,
     createdAt: '2021-01-03T00:00:00Z',

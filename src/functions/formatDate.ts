f1932a5d052c0e5c0a7404319b4099096bed03d4
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-18 07:33:31 +0900

    :sparkles: 日付のフォーマット関数を作成

---
+++ src/functions/formatDate.ts
@@ -0,0 +1,12 @@
+const formatDate = (isoString: string): string => {
+  const date = new Date(isoString)
+  // TODO: 正規表現を用いてisoStringが正しい形式かチェックする
+
+  const year = date.getFullYear()
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const day = date.getDate().toString().padStart(2, '0')
+
+  return `${year}/${month}/${day}`
+}
+
+export default formatDate

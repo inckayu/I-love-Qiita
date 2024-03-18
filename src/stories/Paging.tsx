99536ff324d8914c2329ede6c7a85fdb5863938d
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-18 12:39:24 +0900

    :lipstick: 記事一覧画面のページング部分にLinkTextButttonコンポーネントを使用

--- src/stories/Paging.tsx
+++ src/stories/Paging.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import styles from '../styles/modules/paging.module.scss'
+import LinkTextButton from './LinkTextButton'
 
 const Paging = () => {
   // TODO: ページングの実装
   // TODO: リンクテキストのコンポーネント作成
   return (
     <div className={styles.paging}>
-      <div>prev</div>
-      <div>xx/xx</div>
-      <div>next</div>
+      <LinkTextButton text="prev" />
+      <div className={styles.paging__page}>xx/xx</div>
+      <LinkTextButton text="next" />
     </div>
   )
 }

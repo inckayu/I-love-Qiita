5ed7b40a32f91b606f9488a08be290616d9880ad
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-18 11:48:40 +0900

    :building_construction: 記事詳細画面のHTMLのみ作成

--- src/pages/[articleId].tsx
+++ src/pages/[articleId].tsx
@@ -4,6 +4,12 @@ import axios from 'axios'
 import { Article } from '@/types/Article'
 import { qiitaApiTokenState } from '@/state/qiitaApiTokenState'
 import { useRecoilValue } from 'recoil'
+import Tag from '@/stories/Tag'
+import UserInfo from '@/stories/UserInfo'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import BookmarkIcon from '@mui/icons-material/Bookmark'
+import Divider from '@/stories/Divider'
+import { Button } from '@/stories/Button'
 
 const Article = () => {
   const [article, setArticle] = useState<Article | null>(null)
@@ -32,16 +38,51 @@ const Article = () => {
       })
     }
   }, [router.query.articleId])
+
+  const tags = ['typescript', 'react', 'nextjs']
   return article === null ? (
     <div>loading...</div>
   ) : (
     <div>
-      <div>{article.title}</div>
-      <div>{article.created_at}</div>
-      <div>{article.updated_at}</div>
-      <div>{article.url}</div>
-      <div>{article.user.id}</div>
-      <div>{article.body}</div>
+      <div>back</div>
+      <div>
+        <div>
+          <div>{article.title}</div>
+          <Button variant="primary" size="large" label="Read in Qiita" />
+        </div>
+
+        <div>
+          {tags.map((tag, index) => (
+            <Tag key={index} tag={tag} />
+          ))}
+        </div>
+        <div>
+          <div>
+            <div>{article.created_at}</div>
+            <div>{article.updated_at}</div>
+          </div>
+          <UserInfo
+            user={{
+              organization: article.user.organization,
+              name: article.user.name,
+              profile_image_url: article.user.profile_image_url,
+            }}
+          />
+        </div>
+        <div>
+          <div>
+            <FavoriteIcon />
+            <div>{article.likes_count}</div>
+          </div>
+          <div>
+            <BookmarkIcon />
+            <div>{article.stocks_count}</div>
+          </div>
+        </div>
+        <Divider />
+        {/* TODO: dangerouslySetInnerHTMLにセットしたrendered_bodyのサニタイズ */}
+        <div dangerouslySetInnerHTML={{ __html: article.rendered_body }} />
+      </div>
     </div>
   )
 }

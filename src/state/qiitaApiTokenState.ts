38407700280cb72dbddd4440043c8da549ae7144
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-19 12:28:08 +0900

    :passport_control: 開発環境でAPIキーをデフォルトで設定するか選択できるようにした

--- src/state/qiitaApiTokenState.ts
+++ src/state/qiitaApiTokenState.ts
@@ -3,7 +3,8 @@ import { atom } from 'recoil'
 export const qiitaApiTokenState = atom<string>({
   key: 'qiitaApiTokenState',
   default:
-    process.env.NODE_ENV === 'development'
+    process.env.NODE_ENV === 'development' &&
+    process.env.NEXT_PUBLIC_QIITA_TOKEN === 'true'
       ? process.env.NEXT_PUBLIC_QIITA_TOKEN
       : '',
 })

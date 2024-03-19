f14eefc6ba3bbf655f0f977b863da5db830f5f52
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-20 00:48:37 +0900

    :recycle: index.tsxから不要な記述を削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from 'react'
 import Head from 'next/head'
-import axios from 'axios'
-import { useRecoilState, useRecoilValue } from 'recoil'
+import { useRecoilValue } from 'recoil'
 import { qiitaApiTokenState } from '@/state/qiitaApiTokenState'
 import { articleTitleState } from '@/state/articleTitleState'
 import { Button } from '@/stories/Button'
@@ -17,43 +15,22 @@ import Paging from '@/stories/Paging'
 import CommonModal from '@/stories/CommonModal'
 import { isOpenApiKeyModalState } from '@/state/isOpenModalState'
 import ApiKeyForm from '@/stories/ApiKeyForm'
-import { generateAbstract } from '@/functions/generateAbstract'
-import { fetchArticles } from '@/functions/fetchArticles'
 import { articlesState } from '@/state/articlesState'
 import { isSearchingState } from '@/state/isSearchingState'
 import { generatedAbstractsState } from '@/state/generatedAbstracts'
 import useSearchForm from '@/hooks/useSearchForm'
+import { isValidApiKeyTokenState } from '@/state/isValidApiTokenState'
 
 export default function Home() {
-  const [articles, setArticles] = useRecoilState<Article[]>(articlesState)
-  const [isSearching, setIsSearching] =
-    useRecoilState<boolean>(isSearchingState)
-  const [isValidApiToken, setIsValidApiToken] = useState<boolean>(false)
-  const [generatedAbstracts, setGeneratedAbstracts] = useRecoilState<string[]>(
-    generatedAbstractsState
-  )
+  const articles = useRecoilValue<Article[]>(articlesState)
+  const isSearching = useRecoilValue<boolean>(isSearchingState)
+  const isValidApiKeyToken = useRecoilValue<boolean>(isValidApiKeyTokenState)
+  const generatedAbstracts = useRecoilValue<string[]>(generatedAbstractsState)
   const qiitaApiToken = useRecoilValue<string>(qiitaApiTokenState)
   const articleTitle = useRecoilValue<string>(articleTitleState)
   const isOpenApiKeyModal = useRecoilValue<boolean>(isOpenApiKeyModalState)
   const { handleApiKeyModalClose, handleApiKeyButton, handleTitleClick } =
     useSearchForm()
-
-  useEffect(() => {
-    setIsValidApiToken(true)
-    if (articleTitle.length && qiitaApiToken.length) {
-      // 記事詳細画面から戻ってきた場合のみ実行されることを想定しているのでisValidApiTokenはチェックしない
-      fetchArticles(articleTitle, qiitaApiToken).then(async (articles) => {
-        setArticles(articles)
-
-        const summaries = await Promise.all(
-          articles.map((article) => generateAbstract(article.body))
-        )
-        console.log(summaries)
-        setGeneratedAbstracts(summaries) // 要約の配列を状態に保存
-        setIsSearching(false)
-      })
-    }
-  }, [])
 
   return (
     <>
@@ -90,7 +67,7 @@ export default function Home() {
                 disabled={
                   !articleTitle.length ||
                   !qiitaApiToken.length ||
-                  !isValidApiToken
+                  !isValidApiKeyToken
                 }
               />
             </div>

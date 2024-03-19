6d3743376a76f920aa203a18cdb90a1df4bdb521
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-19 12:30:46 +0900

    :pencil2: 環境変数の名前を修正

--- src/state/qiitaApiTokenState.ts
+++ src/state/qiitaApiTokenState.ts
@@ -4,7 +4,7 @@ export const qiitaApiTokenState = atom<string>({
   key: 'qiitaApiTokenState',
   default:
     process.env.NODE_ENV === 'development' &&
-    process.env.NEXT_PUBLIC_QIITA_TOKEN === 'true'
+    process.env.NEXT_PUBLIC_DEFAULT_QIITA_TOKEN === 'true'
       ? process.env.NEXT_PUBLIC_QIITA_TOKEN
       : '',
 })

ae7188b1981c8816d3cc5a6f0e830a917509a03d
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-21 01:15:53 +0900

    :lipstick: スケルトンスクリーンのコンポーネントを作成

---
+++ src/stories/SkeletonArticleCard.tsx
@@ -0,0 +1,25 @@
+import styles from '../styles/modules/articlecard.module.scss'
+import SkeletonUserInfo from './SkeletonUserInfo'
+
+const SkeletonArticleCard = () => {
+  return (
+    <div className={styles.skeleton}>
+      <div className={styles.skeleton__left}>
+        <div>
+          <div className={styles.skeleton__title} />
+          <div className={styles.skeleton__date} />
+        </div>
+        <SkeletonUserInfo />
+      </div>
+      <div className={styles.skeleton__wrapper}>
+      <div className={styles.skeleton__right} />
+      <div className={styles.skeleton__right} />
+      <div className={styles.skeleton__right} />
+      <div className={styles.skeleton__right} />
+      </div>
+      
+    </ div>
+  )
+}
+
+export default SkeletonArticleCard

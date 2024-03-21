aa0f54d3b9ae4353bc33b9a0bcdacea4a038a576
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-21 10:36:35 +0900

    :truck: 記事詳細画面の記事の本文以外の部分を別のコンポーネントに分けた

--- src/pages/[articleId].tsx
+++ src/pages/[articleId].tsx
@@ -1,26 +1,19 @@
-import BookmarkIcon from '@mui/icons-material/Bookmark'
-import FavoriteIcon from '@mui/icons-material/Favorite'
 import CircularProgress from '@mui/material/CircularProgress';
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { useRecoilValue } from 'recoil'
 import sanitizeHtml from 'sanitize-html'
 
 import { fetchArticle } from '@/functions/fetchArticle'
-import formatDate from '@/functions/formatDate'
-import { getUserName } from '@/functions/getUserName'
 
-import { Button } from '@/stories/Button'
 import Divider from '@/stories/Divider'
 import LinkText from '@/stories/LinkText'
-import Tag from '@/stories/Tag'
-import UserInfo from '@/stories/UserInfo'
 
 import { Article } from '@/types/Article'
 
 import { qiitaApiTokenState } from '@/state/qiitaApiTokenState'
 import styles from '@/styles/modules/detailedarticle.module.scss'
+import DetailedArticleHeader from '@/stories/DetailedArticleHeader';
 
 
 const DetailedArticle = () => {
@@ -58,48 +51,7 @@ const DetailedArticle = () => {
       </div>
       <div className={styles.detailedarticle__wrapper}>
         <div>
-          <div className={styles.detailedarticle__top}>
-            <div className={styles.detailedarticle__title}>{article.title}</div>
-            <Link href={article.url} rel="noopener noreferrer" target="_blank">
-              <Button variant="primary" size="large" label="Read in Qiita" />
-            </Link>
-          </div>
-
-          <div className={styles.detailedarticle__tags}>
-            {article.tags
-              ? article.tags.map((tag, index) => (
-                  <div className={styles.detailedarticle__tagwrapper} key={index}>
-                    <Tag tag={tag.name} />
-                  </div>
-                ))
-              : null}
-          </div>
-          <div className={styles.detailedarticle__info}>
-            <div className={styles.detailedarticle__times}>
-              <div>投稿：{formatDate(article.created_at)}</div>
-              <div>更新：{formatDate(article.updated_at)}</div>
-            </div>
-            <UserInfo
-              user={{
-                organization: article.user.organization,
-                name: getUserName({
-                  name: article.user.name,
-                  id: article.user.id,
-                }),
-                profile_image_url: article.user.profile_image_url,
-              }}
-            />
-          </div>
-          <div className={styles.detailedarticle__icons}>
-            <div className={styles.detailedarticle__icon}>
-              <FavoriteIcon />
-              <div>{article.likes_count}</div>
-            </div>
-            <div className={styles.detailedarticle__icon}>
-              <BookmarkIcon />
-              <div>{article.stocks_count}</div>
-            </div>
-          </div>
+          <DetailedArticleHeader article={article} />
           <div className={styles.detailedarticle__divider}>
             <Divider />
           </div>

42808897f795288e7d12ece10837f36397621190
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-22 17:58:12 +0900

    :lipstick: 詳細検索モーダルのUIのみ作成

--- src/stories/DetailedSearchForm.tsx
+++ src/stories/DetailedSearchForm.tsx
@@ -1,21 +1,50 @@
-import useDetailedSearchForm from "@/hooks/useDetailedSearchForm"
+import useDetailedSearchForm from '@/hooks/useDetailedSearchForm'
 
-import { Button } from "./Button"
-import styles from "../styles/modules/detailedsearchform.module.scss"
+import { Button } from './Button'
+import TagsAutoComplete from './TagsAutoComplete'
+import TextBox from './TextBox'
+import styles from '../styles/modules/detailedsearchform.module.scss'
 
-const DetailedSearchForm = () => {
+export default function DetailedSearchForm() {
   const { handleCancelButtonClick, handleOKButtonClick } = useDetailedSearchForm()
+
   return (
     <div className={styles.detailedsearchform}>
-      <div>aaa</div>
-      <div>aaa</div>
-      <div>aaa</div>
+      <h2 className={styles.detailedsearchform__title}>Detailed Search Options</h2>
+      <div className={styles.detailedsearchform__textboxes}>
+        <TextBox value="" label="Title" placeholder="Keywords in the article body" />
+        <TextBox value="" label="body" placeholder="Keywords in the article body" />
+        <TextBox value="" label="Author" placeholder="Keywords in the article body" />
+      </div>
+      <div className={styles.detailedsearchform__ranges}>
+        <div>
+          <div className={styles.detailedsearchform__rangename}>Publication Range</div>
+          <div className={styles.detailedsearchform__range}>
+            <TextBox value="" label="Start" width={104} placeholder="yyyy/MM/dd" />
+            <TextBox value="" label="End" width={104} placeholder="yyyy/MM/dd" />
+          </div>
+        </div>
+
+        <div>
+          <div className={styles.detailedsearchform__rangename}>Last Update Range</div>
+          <div className={styles.detailedsearchform__range}>
+            <TextBox value="" label="Start" width={104} placeholder="yyyy/MM/dd" />
+            <TextBox value="" label="End" width={104} placeholder="yyyy/MM/dd" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.detailedsearchform__tag}>
+        <TagsAutoComplete label="Tags" />
+        <TagsAutoComplete label="Tags to Exclude" />
+      </div>
       <div className={styles.detailedsearchform__buttons}>
-        <Button label="Cancel" variant="secondary" onClick={handleCancelButtonClick} />
-        <Button label="OK" onClick={handleOKButtonClick} />
+        <div>
+          <Button label="Cancel" variant="secondary" onClick={handleCancelButtonClick} />
+        </div>
+        <div>
+          <Button label="OK" onClick={handleOKButtonClick} />
+        </div>
       </div>
     </div>
   )
 }
-
-export default DetailedSearchForm

2ad57f7bc68ad317885cdcd117bba3e7e7147eac
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-23 21:06:15 +0900

    :bento: MUIのカスタマイズファイルを作成して最上位のコンポーネントで読み込んだ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
-// import "@/styles/globals.css";
+import { ThemeProvider } from '@mui/material'
 import { RecoilRoot } from 'recoil'
-
+import { theme } from '@/constants/mui'
 import type { AppProps } from 'next/app'
-
 import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </RecoilRoot>
   )
 }

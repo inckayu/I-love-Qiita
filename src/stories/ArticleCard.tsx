19ce735a799c50007cb29dbe6a95e43468cea9af
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-23 22:30:18 +0900

    :lipstick: 検索ワードと一致するタイトルの部分をハイライト

--- src/stories/ArticleCard.tsx
+++ src/stories/ArticleCard.tsx
@@ -10,6 +10,11 @@ import SkeletonArticleCard from './SkeletonArticleCard'
 import UserInfo from './UserInfo'
 import styles from '../styles/modules/articlecard.module.scss'
 
+import { articleTitleState } from '@/state/articleQuery'
+import { useRecoilValue } from 'recoil'
+import sanitizeHtml from 'sanitize-html'
+import { titleHighlighter } from '@/functions/titleHighlighter'
+
 interface ArticleCardProps {
   article: Article
   summary: string
@@ -17,13 +22,21 @@ interface ArticleCardProps {
 }
 
 const ArticleCard = ({ article, summary, isSkeleton }: ArticleCardProps) => {
+  const articleTitle = useRecoilValue<string>(articleTitleState)
+  const words = articleTitle.split(/[\u{20}\u{3000}]/u)
+
   return isSkeleton ? (
     <SkeletonArticleCard />
   ) : (
     <Link key={article.id} href={article.id} className={styles.articlecard}>
       <div className={styles.articlecard__left}>
         <div>
-          <div className={styles.articlecard__title}>{article.title}</div>
+          <div
+            className={styles.articlecard__title}
+            dangerouslySetInnerHTML={{
+              __html: titleHighlighter(sanitizeHtml(article.title), words),
+            }}
+          />
           <div className={styles.articlecard__date}>{formatDate(article.created_at)}</div>
         </div>
         <UserInfo

32250ae82ed5bc4451b864c86a9579ce794a8f5b
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-23 18:55:44 +0900

    :lipstick: 記事詳細画面でタグの画像を表示できるようにした

--- src/stories/Tag.tsx
+++ src/stories/Tag.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+
+import { fetchTag } from '@/functions/fetchTag'
 
 import styles from '../styles/modules/tag.module.scss'
 
@@ -7,7 +10,28 @@ interface TagProps {
 }
 
 const Tag = ({ tag }: TagProps) => {
-  return <div className={styles.tag}>{tag}</div>
+  const [tagImage, setTagImage] = useState<string | null>(null)
+
+  useEffect(() => {
+    fetchTag(tag)
+      .then((result) => {
+        setTagImage(result.icon_url)
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+  }, [])
+
+  return (
+    <div className={styles.tag}>
+      {tagImage ? (
+        <div className={styles.tag__image}>
+          <Image src={tagImage} width={16} height={16} alt={tag} />
+        </div>
+      ) : null}
+      <div>{tag}</div>
+    </div>
+  )
 }
 
 export default Tag

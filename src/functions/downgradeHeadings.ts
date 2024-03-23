e129a104f0b4716da09ff3462dbf7b5128413341
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-24 03:18:48 +0900

    :fire: 不要なconsole.logを削除

--- src/functions/downgradeHeadings.ts
+++ src/functions/downgradeHeadings.ts
@@ -1,10 +1,7 @@
 export const downgradeHeadings = (body: string) => {
-  console.log(body)
   body = body.replace(
     /<h([1-5])((?:\s+[^>]*?)?)>((?:.|[\r\n])*?)<\/h\1>/g,
     (_, p1, p2, p3) => `<h${Number(p1) + 1}${p2}>${p3}</h${Number(p1) + 1}>`
   )
-  console.log(body)
-
   return body
 }

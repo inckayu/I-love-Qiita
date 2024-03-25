7691635d5b20f906f844091696cbbb2b93acc6f1
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-26 03:57:44 +0900

    :ambulance: metaタグのurlを本番環境のものに変更

--- src/components/CommonHead.tsx
+++ src/components/CommonHead.tsx
@@ -27,7 +27,7 @@ const CommonHead = ({ genre, title, author, icon, publishedAt }: CommonHeadProps
         content="I love Qiita, where many seekers of knowledge meet up."
       />
       <meta property="og:type" content={genre} />
-      <meta property="og:url" content="http://localhost:3000" />
+      <meta property="og:url" content="https://i-love-qiita.vercel.app" />
 
       <meta property="og:site_name" content="I love Qiita" />
       <meta property="og:locale" content="ja_JP" />
@@ -38,17 +38,17 @@ const CommonHead = ({ genre, title, author, icon, publishedAt }: CommonHeadProps
         <>
           <meta
             property="og:image"
-            content={`http://localhost:3000/api/ogpArticle?title=${title}&author=${author}&icon=${icon}&publishedAt=${publishedAt}`}
+            content={`https://i-love-qiita.vercel.app/api/ogpArticle?title=${title}&author=${author}&icon=${icon}&publishedAt=${publishedAt}`}
           />
           <meta
             name="twitter:image"
-            content={`http://localhost:3000/api/ogpArticle?title=${title}&author=${author}&icon=${icon}&publishedAt=${publishedAt}`}
+            content={`https://i-love-qiita.vercel.app/api/ogpArticle?title=${title}&author=${author}&icon=${icon}&publishedAt=${publishedAt}`}
           />
         </>
       ) : (
         <>
-          <meta property="og:image" content="http://localhost:3000/api/ogp" />
-          <meta name="twitter:image" content="http://localhost:3000/api/ogp" />
+          <meta property="og:image" content="https://i-love-qiita.vercel.app/api/ogp" />
+          <meta name="twitter:image" content="https://i-love-qiita.vercel.app/api/ogp" />
         </>
       )}
     </Head>

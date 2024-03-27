f73d663ff8d4412f1e5960757622316ae2c8817f
片岡勇輝 <kayuyuuki0825@icloud.com>
2024-03-27 11:09:07 +0900

    :fire: 不要なconsole.logとコメントを削除

--- src/functions/fetchArticles.ts
+++ src/functions/fetchArticles.ts
@@ -7,8 +7,6 @@ export const fetchArticles = async (
   token: string,
   page: number
 ): Promise<Article[]> => {
-  // TODO: 詳細検索機能の実装後、検索条件はオブジェクトとかにまとめて引数として渡すようにする
-
   const config = {
     headers: {
       'Content-Type': 'application/json',
@@ -19,6 +17,5 @@ export const fetchArticles = async (
     `https://qiita.com/api/v2/items?page=${page}&per_page=10${query.length ? `&query=${query}` : ''}`,
     config
   )
-  console.log(res)
   return res.data
 }
